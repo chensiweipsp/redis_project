aedc49d9d7bb9e2becd0747bde14ce7e60c0e050
chensiweipsp <295255658@qq.com>
2018-05-09 03:51:21 +0800

    添加了课堂DEMO

--- src/main/java/com/luo/redis/cache/MethodCacheInterceptor.java
+++ src/main/java/com/luo/redis/cache/MethodCacheInterceptor.java
@@ -10,42 +10,13 @@ import org.springframework.data.redis.core.ValueOperations;
 public class MethodCacheInterceptor implements MethodInterceptor {
 	
 	private RedisTemplate<Serializable, Object> redisTemplate;
+	
 	private Long defaultCacheExpireTime = 10l; // 缓存默认的过期时间,这里设置了10秒
 	
 	public Object invoke(MethodInvocation invocation) throws Throwable {
-		Object value = null;
-
-		String targetName = invocation.getThis().getClass().getName();
-		String methodName = invocation.getMethod().getName();
-
-		Object[] arguments = invocation.getArguments();
-		String key = getCacheKey(targetName, methodName, arguments);
-
-		try {
-			// 判断是否有缓存
-			if (exists(key)) {
-				return getCache(key);
-			}
-			// 写入缓存
-			value = invocation.proceed();
-			if (value != null) {
-				final String tkey = key;
-				final Object tvalue = value;
-				new Thread(new Runnable() {
-					public void run() {
-						setCache(tkey, tvalue, defaultCacheExpireTime);
-					}
-				}).start();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (value == null) {
-				return invocation.proceed();
-			}
-		}
-		return value;
+		
+		return null;
 	}
-
 	/**
 	 * 创建缓存key
 	 *
@@ -64,8 +35,7 @@ public class MethodCacheInterceptor implements MethodInterceptor {
 		}
 		return sbu.toString();
 	}
-
-
+	
 	
 	/**
 	 * 判断缓存中是否有对应的value
@@ -76,7 +46,6 @@ public class MethodCacheInterceptor implements MethodInterceptor {
 	public boolean exists(final String key) {
 		return redisTemplate.hasKey(key);
 	}
-
 	/**
 	 * 读取缓存
 	 * 
@@ -90,9 +59,6 @@ public class MethodCacheInterceptor implements MethodInterceptor {
 		result = operations.get(key);
 		return result;
 	}
-
-
-
 	/**
 	 * 写入缓存
 	 * 
@@ -102,21 +68,33 @@ public class MethodCacheInterceptor implements MethodInterceptor {
 	 */
 	public boolean setCache(final String key, Object value, Long expireTime) {
 		boolean result = false;
+		
 		try {
+			
 			ValueOperations<Serializable, Object> operations = redisTemplate
 					.opsForValue();
+			
 			operations.set(key, value);
+			
 			redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+			
+			
 			result = true;
-		} catch (Exception e) {
+		
+		
+		}
+		
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		return result;
-	}
 
+		return result;
+		
+	}
+	
+	
 	public void setRedisTemplate(
 			RedisTemplate<Serializable, Object> redisTemplate) {
 		this.redisTemplate = redisTemplate;
 	}
 }
-
